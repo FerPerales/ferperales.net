0ac2786e7e976f203df4e02d23c590ba8c82ca8e
Fer Perales <fer.perales.montoya@gmail.com>
2012-12-04 14:44:42 -0600

    Adds php files

---
+++ php/footer.php
@@ -0,0 +1,32 @@
+<footer class="row well footer">
+	<div class="span12">
+		<div class="row centered-text white-text">
+			<div class="span3 offset1">
+				<p>Made with love using 
+					<a href="http://github.com">Github</a> + 
+					<a href="http://twitter.github.com/bootstrap/">Bootstrap</a> + 
+					<a href="http://p3k.org/rss/">p3k RSS widget</a> + 
+					<a href="http://www.addthis.com">addThis social bar</a> + 
+					<a href="http://www.wbotelhos.com/raty/">Raty plugin</a>
+				</p>
+			</div>
+			<div class="span3 offset1 white-text">
+				<p>							
+					<img src="img/mail.jpg" alt="me@thisdomain.net">													
+				</p>
+			</div>
+		
+			<div class="span3 offset1 white-text">
+				<div class="addthis_toolbox addthis_32x32_style addthis_default_style">
+					<a class="addthis_button_facebook_follow" addthis:userid="FerPeralesm"></a>
+					<a class="addthis_button_twitter_follow" addthis:userid="FerPeralesM"></a>
+					<a class="addthis_button_linkedin_follow" addthis:userid="Ferperales"></a>
+					<a class="addthis_button_google_follow" addthis:userid="108653816095447062070"></a>
+					<a class="addthis_button_youtube_follow" addthis:userid="FerPeralesM"></a>
+					<a class="addthis_button_rss_follow" addthis:url="http://ferperales.wordpress.com/feed"></a>
+					</div>
+				<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-5016116372fca99c"></script>				
+			</div>
+		</div>
+	</div>			
+</footer>
