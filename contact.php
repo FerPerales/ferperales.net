c128ee666a475f234ebd81fed998f1ba3303c174
Fer Perales <fer.perales.montoya@gmail.com>
2012-12-07 20:38:59 -0600

    Deleted and rearranged contact information

--- contact.php
+++ contact.php
@@ -28,38 +28,6 @@
 						First of all, I appreciate your taking the time to visit my webpage. 	
 					</p>
 					<p>
-						The easiest way to get in contact is a commet via disqus in the form below
-					</p>											
-					<p>
-						<div id="disqus_thread"></div>
-					</p>
-					<p>
-						If you need some help with your project or just want to say 'Hi!', feel free to send me an email and I'll do my best in order to help you.
-						furthermore, we can have a real-time chat via gTalk whenever I'm available. 
-					</p>					
-					<p>
-							<p>							
-								<img src="img/mail.jpg" alt="me@thisdomain.net">													
-							</p>							
-					</p>
-					<p>
-						<iframe src="http://www.google.com/talk/service/badge/Show?tk=z01q6amlqrdp71hshr4ohks97q59mjvvtl1grd92ovc7u02cf91qiq7ufr8rasp1r0elsfk4i129tqnnd545e34mf7si5ja59ljhpf3fnbqa7dcdlcps4qv71ufh2357bg191gnueml6tokpl29uug5us08hhsn76ochj95at64el028md1rdpn9ubv58r1hin4&amp;w=300&amp;h=18" frameborder="0" allowtransparency="true" width="300" height="18"></iframe>
-					</p>											
-					<p>
-						You can also add me to your favorite social network:
-					</p>
-					<p>					
-						<div class="addthis_toolbox addthis_32x32_style addthis_default_style">							
-								<a class="addthis_button_facebook_follow" addthis:userid="FerPeralesm"></a>
-								<a class="addthis_button_twitter_follow" addthis:userid="FerPeralesM"></a>
-								<a class="addthis_button_linkedin_follow" addthis:userid="Ferperales"></a>
-								<a class="addthis_button_google_follow" addthis:userid="108653816095447062070"></a>
-								<a class="addthis_button_youtube_follow" addthis:userid="FerPeralesM"></a>
-								<a class="addthis_button_rss_follow" addthis:url="http://ferperales.wordpress.com/feed"></a>						
-						</div>
-					</p>
-					<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-5016116372fca99c"></script>
-					<p>
 						If you want me to colaborate in your project, you can send me and email, add me to gTalk or add me to Skype to make an agreement.
 						I'd love to work with you ;)							
 					</p>
@@ -76,8 +44,6 @@
 
 									
 		</div>	
-		
-
         <script type="text/javascript">
             /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
             var disqus_shortname = 'ferperalesblog'; // required: replace example with your forum shortname
@@ -91,10 +57,9 @@
         </script>
         <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
         <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-        			
+
 <?php
 	require 'php/footer.php';
 ?>
-
 	</body>
 </html>
