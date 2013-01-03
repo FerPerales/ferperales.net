bf3ca3471ad9c99f372179d11f01da82b3f7b541
Fer Perales <fer.perales.montoya@gmail.com>
2013-01-03 11:01:04 -0600

    Fix RSS URL

--- php/footer.php
+++ php/footer.php
@@ -28,7 +28,7 @@ echo 'Latest update: '.date ("F d Y H:i:s", filemtime($filename));
 					<a class="addthis_button_linkedin_follow" addthis:userid="Ferperales"></a>
 					<a class="addthis_button_google_follow" addthis:userid="108653816095447062070"></a>
 					<a class="addthis_button_youtube_follow" addthis:userid="FerPeralesM"></a>
-					<a class="addthis_button_rss_follow" addthis:url="http://ferperales.net/blog/feed"></a>
+					<a class="addthis_button_rss_follow" addthis:url="http://blog.ferperales.net/feed"></a>
 				</div>
 				<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=ra-5016116372fca99c"></script>				
 			</div>
